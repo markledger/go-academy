dee0a905232041b7fe692343942aefc729b781a7
Mark Ledger <mark.ledger@bjss.com>
2025-04-02 13:22:21 +0100

    Added id and action flag and moved flags to init()

--- cmdlineapp/main.go
+++ cmdlineapp/main.go
@@ -16,6 +16,16 @@ import (
 var pl = fmt.Println
 var filePath = "./todo-list.txt"
 
+var id int
+var action string
+var task string
+
+func init() {
+	flag.IntVar(&id, "id", 0, "use in combination with the -action flag to select task to be modified")
+	flag.StringVar(&action, "action", "create", "use in combination with -id. Select action from: edit|delete ")
+	flag.StringVar(&task, "task", "example task to complete", "the task you want to create, or the new task if editing")
+}
+
 func parseFileToSlice(filePath string) []string {
 	var data []string
 
@@ -55,7 +65,6 @@ func appendFile(filePath string, todo string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer f.Close()
 
 	if _, err := f.WriteString(todo + "\n"); err != nil {
@@ -65,40 +74,17 @@ func appendFile(filePath string, todo string) {
 
 func main() {
 	var todoList []string
-	taskPrt := flag.String("task", "Consume double espresso", "a string")
 	flag.Parse()
 
 	todoList = parseFileToSlice(filePath)
-	todoList = append(todoList, *taskPrt)
+	todoList = append(todoList, task)
+	pl(action)
+	pl(id)
+	pl(task)
+	pl("id | Task")
 
 	for i, todo := range todoList {
 		pl(fmt.Sprintf("[%d]: %s", i+1, todo))
 	}
-	appendFile(filePath, *taskPrt)
-
-	//file, err := os.OpenFile(filePath, os.O_RDONLY|os.O_CREATE, 0644)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer file.Close()
-	//
-	//if _, err := file.WriteString(*taskPrt + "\n"); err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//for _, i := range args {
-	//	val, err := strconv.Atoi(i)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	iArgs = append(iArgs, val)
-	//}
-	//
-	//max := 0
-	//for _, val := range iArgs {
-	//	if val > max {
-	//		max = val
-	//	}
-	//}
-	//pl("Max Value :", max)
+	appendFile(filePath, task)
 }
