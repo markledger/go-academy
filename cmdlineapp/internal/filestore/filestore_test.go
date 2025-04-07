4a487582f180556bf00172125c821024e425a63e
Mark Ledger <mark.ledger@bjss.com>
2025-04-07 14:17:02 +0100

    Added test to ensure file is parsed to slice corectly

--- cmdlineapp/internal/filestore/filestore_test.go
+++ cmdlineapp/internal/filestore/filestore_test.go
@@ -5,14 +5,25 @@ import (
 )
 
 func TestFileIsParsedToSlice(t *testing.T) {
-	data, err := ParseFileToSlice("./internal/filestore/test_data/database.txt")
+	data, err := ParseFileToSlice("./test_data/database.txt")
 
 	if err != nil {
 		t.Error(err)
 	}
 
 	if len(data) != 3 {
-		t.Error("")
+		t.Error("Includes empty values")
 	}
 
+	if data[0] != "Fish and Chips" {
+		t.Error("Slice element 0 incorrect")
+	}
+
+	if data[1] != "Jerk Chicken" {
+		t.Error("Slice element 1 incorrect")
+	}
+
+	if data[2] != "Fish and Crab" {
+		t.Error("Slice element 2 incorrect")
+	}
 }
