52890c214eb4eb7646ead1f1e7284d588f1ea0b4
Mark Ledger <mark.ledger@bjss.com>
2025-04-08 11:44:18 +0100

    Tested validate and handle function|

--- cmdlineapp/main_test.go
+++ cmdlineapp/main_test.go
@@ -88,3 +88,80 @@ func TestValidateFlagsRequiresValidIdAndTaskContentToEdit(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestHandleDeleteAction(t *testing.T) {
+	todoList := []string{"Meet the Klingons", "Resist the Borg", "Defeat the Dominion"}
+	action = "delete"
+	id = 2
+
+	updatedTodoList, _ := handleAction(todoList)
+
+	if len(updatedTodoList) != 2 {
+		t.Error()
+	}
+
+	if updatedTodoList[0] != "Meet the Klingons" {
+		t.Error()
+	}
+
+	if updatedTodoList[1] != "Defeat the Dominion" {
+		t.Error()
+	}
+
+}
+
+func TestHandleEditAction(t *testing.T) {
+	todoList := []string{"Meet the Klingons", "Resist the Borg", "Defeat the Dominion"}
+	action = "edit"
+	id = 2
+	task = "Beat the Borg"
+
+	updatedTodoList, _ := handleAction(todoList)
+
+	if len(updatedTodoList) != 3 {
+		t.Error()
+	}
+
+	if updatedTodoList[0] != "Meet the Klingons" {
+		t.Error()
+	}
+
+	if updatedTodoList[1] != "Beat the Borg" {
+		t.Error()
+	}
+
+	if updatedTodoList[2] != "Defeat the Dominion" {
+		t.Error()
+	}
+
+}
+
+func TestHandleCreateAction(t *testing.T) {
+	todoList := []string{"Meet the Klingons", "Resist the Borg", "Defeat the Dominion"}
+
+	action = "create"
+	task = "Run away"
+	appendedTodoList, err := handleAction(todoList)
+	if len(appendedTodoList) != 4 {
+		t.Error("There should be 4 elements in the slice")
+	}
+	if appendedTodoList[0] != "Meet the Klingons" {
+		t.Error()
+	}
+
+	if appendedTodoList[1] != "Resist the Borg" {
+		t.Error()
+	}
+
+	if appendedTodoList[2] != "Defeat the Dominion" {
+		t.Error()
+	}
+
+	if appendedTodoList[3] != "Run away" {
+		t.Error()
+	}
+
+	if err != nil {
+		t.Error()
+	}
+}
